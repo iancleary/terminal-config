5117df61c94aada9ab31046143d50626dddb712e
iancleary <github@iancleary.me>
2024-08-23 15:57:54 -0700

    Update nvim to remove deprecated functions, attempt1

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,23 +1,23 @@
 vim.g.mapleader = " "
 
 -- Line numbering
-vim.api.nvim_win_set_option(0, "number", true)
-vim.api.nvim_win_set_option(0, "relativenumber", true)
-vim.api.nvim_win_set_option(0, "wrap", false)
+vim.api.nvim_set_option_value("number", true, {})
+vim.api.nvim_set_option_value("relativenumber", true. {})
+vim.api.nvim_set_option_value("wrap", false, {})
 
 -- Better Markdown
-vim.api.nvim_set_option("conceallevel", 0)
+vim.api.nvim_set_option_value("conceallevel", 0, {})
 
 -- Search case
-vim.api.nvim_set_option("ignorecase", true)
-vim.api.nvim_set_option("smartcase", true)
+vim.api.nvim_set_option_value("ignorecase", true, {})
+vim.api.nvim_set_option_value("smartcase", true, {})
 
 -- Hide command line
-vim.api.nvim_set_option("cmdheight", 0)
+vim.api.nvim_set_option_value("cmdheight", 0, {})
 
 -- Minimal number of lines to scroll when the cursor gets off the screen
-vim.api.nvim_set_option("scrolloff", 8)
-vim.api.nvim_set_option("sidescrolloff", 8)
+vim.api.nvim_set_option_value("scrolloff", 8, {})
+vim.api.nvim_set_option_value("sidescrolloff", 8, {})
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
@@ -39,9 +39,9 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- Indents
-vim.api.nvim_set_option("tabstop", 4)
-vim.api.nvim_set_option("shiftwidth", 4)
-vim.api.nvim_set_option("softtabstop", 4)
-vim.api.nvim_set_option("expandtab", true)
-vim.api.nvim_set_option("smartindent", true)
+vim.api.nvim_set_option_value("tabstop", 4, {})
+vim.api.nvim_set_option_value("shiftwidth", 4, {})
+vim.api.nvim_set_option_value("softtabstop", 4, {})
+vim.api.nvim_set_option_value("expandtab", true, {})
+vim.api.nvim_set_option_value("smartindent", true, {})
 vim.cmd("filetype indent plugin on")
