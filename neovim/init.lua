7665792faa94149b3cff2411a611438cf51aeabd
iancleary <github@iancleary.me>
2024-08-23 16:37:59 -0700

    Attempt to pass option_value properly when empty, success

--- neovim/init.lua
+++ neovim/init.lua
@@ -9,15 +9,15 @@ vim.api.nvim_set_option_value("wrap", false, { win = 0 })
 vim.api.nvim_set_option_value("conceallevel", 0, {})
 
 -- Search case
-vim.api.nvim_set_option_value("ignorecase", true, nil)
-vim.api.nvim_set_option_value("smartcase", true, nil)
+vim.api.nvim_set_option_value("ignorecase", true, {})
+vim.api.nvim_set_option_value("smartcase", true, {})
 
 -- Hide command line
-vim.api.nvim_set_option_value("cmdheight", 0, nil)
+vim.api.nvim_set_option_value("cmdheight", 0, {})
 
 -- Minimal number of lines to scroll when the cursor gets off the screen
-vim.api.nvim_set_option_value("scrolloff", 8, nil)
-vim.api.nvim_set_option_value("sidescrolloff", 8, nil)
+vim.api.nvim_set_option_value("scrolloff", 8, {})
+vim.api.nvim_set_option_value("sidescrolloff", 8, {})
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
@@ -39,9 +39,9 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- Indents
-vim.api.nvim_set_option_value("tabstop", 4, nil)
-vim.api.nvim_set_option_value("shiftwidth", 4, nil)
-vim.api.nvim_set_option_value("softtabstop", 4, nil)
-vim.api.nvim_set_option_value("expandtab", true, nil)
-vim.api.nvim_set_option_value("smartindent", true, nil)
+vim.api.nvim_set_option_value("tabstop", 4, {})
+vim.api.nvim_set_option_value("shiftwidth", 4, {})
+vim.api.nvim_set_option_value("softtabstop", 4, {})
+vim.api.nvim_set_option_value("expandtab", true, {})
+vim.api.nvim_set_option_value("smartindent", true, {})
 vim.cmd("filetype indent plugin on")
