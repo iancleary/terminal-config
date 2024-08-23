18dbb2601acd53aaf653ef6e38a8181a52e3c45e
iancleary <github@iancleary.me>
2024-08-23 16:10:05 -0700

    Use nil instead of {}

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,23 +1,23 @@
 vim.g.mapleader = " "
 
 -- Line numbering
-vim.api.nvim_set_option_value("number", true, {})
-vim.api.nvim_set_option_value("relativenumber", true. {})
-vim.api.nvim_set_option_value("wrap", false, {})
+vim.api.nvim_set_option_value("number", true, nil)
+vim.api.nvim_set_option_value("relativenumber", true. nil)
+vim.api.nvim_set_option_value("wrap", false, nil)
 
 -- Better Markdown
-vim.api.nvim_set_option_value("conceallevel", 0, {})
+vim.api.nvim_set_option_value("conceallevel", 0, nil)
 
 -- Search case
-vim.api.nvim_set_option_value("ignorecase", true, {})
-vim.api.nvim_set_option_value("smartcase", true, {})
+vim.api.nvim_set_option_value("ignorecase", true, nil)
+vim.api.nvim_set_option_value("smartcase", true, nil)
 
 -- Hide command line
-vim.api.nvim_set_option_value("cmdheight", 0, {})
+vim.api.nvim_set_option_value("cmdheight", 0, nil)
 
 -- Minimal number of lines to scroll when the cursor gets off the screen
-vim.api.nvim_set_option_value("scrolloff", 8, {})
-vim.api.nvim_set_option_value("sidescrolloff", 8, {})
+vim.api.nvim_set_option_value("scrolloff", 8, nil)
+vim.api.nvim_set_option_value("sidescrolloff", 8, nil)
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
@@ -39,9 +39,9 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- Indents
-vim.api.nvim_set_option_value("tabstop", 4, {})
-vim.api.nvim_set_option_value("shiftwidth", 4, {})
-vim.api.nvim_set_option_value("softtabstop", 4, {})
-vim.api.nvim_set_option_value("expandtab", true, {})
-vim.api.nvim_set_option_value("smartindent", true, {})
+vim.api.nvim_set_option_value("tabstop", 4, nil)
+vim.api.nvim_set_option_value("shiftwidth", 4, nil)
+vim.api.nvim_set_option_value("softtabstop", 4, nil)
+vim.api.nvim_set_option_value("expandtab", true, nil)
+vim.api.nvim_set_option_value("smartindent", true, nil)
 vim.cmd("filetype indent plugin on")
