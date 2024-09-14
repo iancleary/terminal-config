aeb5b7b2bfc09fd89fbb7d62d6a2f700647564d0
iancleary <github@iancleary.me>
2024-09-13 20:10:26 -0700

    Pull in neovim-plugins-overlay abstraction into this repo

--- flake.nix
+++ flake.nix
@@ -1,21 +1,86 @@
 {
-  description = "iancleary's example config";
+  description = "iancleary's terminal configuration (neovim, git, zsh, etc.)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    neovim-plugins = {
-      url = "github:iancleary/neovim-plugins-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
+
+    # neovim-plugins
+    nvim-lspconfig = {
+      url = "github:neovim/nvim-lspconfig";
+      flake = false;
+    };
+    plenary = {
+      url = "github:nvim-lua/plenary.nvim";
+      flake = false;
+    };
+    oceanic-next = {
+      url = "github:mhartington/oceanic-next";
+      flake = false;
+    };
+    telescope = {
+      url = "github:nvim-telescope/telescope.nvim";
+      flake = false;
+    };
+    telescope-file-browser = {
+      url = "github:nvim-telescope/telescope-file-browser.nvim";
+      flake = false;
+    };
+    mini = {
+      url = "github:echasnovski/mini.nvim";
+      flake = false;
+    };
+    indent-blankline = {
+      url = "github:lukas-reineke/indent-blankline.nvim";
+      flake = false;
+    };
+    devicons = {
+      url = "github:nvim-tree/nvim-web-devicons";
+      flake = false;
+    };
+    gitsigns = {
+      url = "github:lewis6991/gitsigns.nvim";
+      flake = false;
+    };
+    dressing = {
+      url = "github:stevearc/dressing.nvim";
+      flake = false;
+    };
+    oil = {
+      url = "github:stevearc/oil.nvim";
+      flake = false;
+    };
+    schemastore = {
+      url = "github:b0o/SchemaStore.nvim";
+      flake = false;
+    };
+    nvim-cmp = {
+      url = "github:hrsh7th/nvim-cmp";
+      flake = false;
+    };
+    cmp-buffer = {
+      url = "github:hrsh7th/cmp-buffer";
+      flake = false;
+    };
+    cmp-nvim-lsp = {
+      url = "github:hrsh7th/cmp-nvim-lsp";
+      flake = false;
+    };
+    cmp-path = {
+      url = "github:hrsh7th/cmp-path";
+      flake = false;
+    };
+    lspkind-nvim = {
+      url = "github:onsails/lspkind-nvim";
+      flake = false;
     };
   };
 
   outputs =
-    inputs @ { self
+    { self
     , nixpkgs
     , nixpkgs-unstable
-    , neovim-plugins
-    }:
+    }@inputs:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
       overlays = {
@@ -23,19 +88,30 @@
           unstable = nixpkgs-unstable.legacyPackages.${prev.system};
           inherit (nixpkgs-unstable.legacyPackages.${prev.system}) neovim-unwrapped;
         };
-        neovimPlugins = neovim-plugins.overlays.default;
+        neovimPlugins = final: prev:
+          let
+            mkPlugin = name: value:
+              prev.pkgs.vimUtils.buildVimPlugin {
+                pname = name;
+                version = value.lastModifiedDate;
+                src = value;
+              };
+            plugins = prev.lib.filterAttrs (name: _: name != "self" && name != "nixpkgs") inputs;
+          in
+          {
+            nvimPlugins = builtins.mapAttrs mkPlugin plugins;
+          };
       };
-
-      legacyPackages = builtins.currentSystem (system:
+    in
+    {
+      legacyPackages = forAllSystems (system:
         import inputs.nixpkgs {
           inherit system;
           overlays = builtins.attrValues overlays;
           config.allowUnfree = true;
         }
       );
-    in
-    {
-      overlays.default = neovim-plugins.overlays.default; # pass through the neovim-plugins overlay
+      overlays.default = overlays.neovimPlugins;
       homeManagerModules.default = import ./default.nix;
       homeManagerModules.home-manager = import ./default.nix;
 
@@ -44,5 +120,21 @@
       });
 
       formatter = forAllSystems (system: nixpkgs.legacyPackages."${system}".nixpkgs-fmt);
+      nixosConfigurations.test = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ({ pkgs, ... }: {
+            boot.isContainer = true;
+            nixpkgs.overlays = overlays;
+            system.stateVersion = "24.05";
+            programs.neovim = {
+              enable = true;
+              configure.packages.myVimPackage = {
+                opt = builtins.attrValues pkgs.nvimPlugins;
+              };
+            };
+          })
+        ];
+      };
     };
 }
