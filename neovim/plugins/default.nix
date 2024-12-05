b317fec70c464f82418aa984597f8131553008dd
iancleary <github@iancleary.me>
2024-12-04 22:02:30 -0700

    disable opts to test

--- neovim/plugins/default.nix
+++ neovim/plugins/default.nix
@@ -17,14 +17,6 @@ let
       opts.scope.enabled = false;
     }
     { plugin = pkgs.nvimPlugins.oceanic-next; }
-    { plugin = pkgs.nvimPlugins.neotree; 
-      opts = {
-        close_if_last_window = false; # -- Close Neo-tree if it is the last window left in the tab
-        popup_border_style = "rounded";
-        enable_git_status = true;
-        enable_diagnostics = true;
-        };
-    }
     { plugin = pkgs.nvimPlugins.nui; }
     { plugin = pkgs.nvimPlugins.devicons; }
     { plugin = pkgs.nvimPlugins.gitsigns; opts = { }; }
@@ -37,6 +29,14 @@ let
       '';
     }
     { plugin = pkgs.vimPlugins.vim-sleuth; }
+    { plugin = pkgs.nvimPlugins.neotree; 
+      # opts = {
+      #   close_if_last_window = false; # -- Close Neo-tree if it is the last window left in the tab
+      #   popup_border_style = "rounded";
+      #   enable_git_status = true;
+      #   enable_diagnostics = true;
+      #   };
+    }
   ];
   lspPlugins = importPlugins [
     ./lspconfig.nix
