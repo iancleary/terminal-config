c5e063166e7f2da1e83aaabac5a396eaad3bf738
iancleary <github@iancleary.me>
2024-12-04 22:05:28 -0700

    Add leader keymap for Neotree

--- neovim/keymaps.lua
+++ neovim/keymaps.lua
@@ -26,3 +26,6 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Navigate to the left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Navigate to the bottom window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Navigate to the top window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Navigate to the right window" })
+
+-- Neotree
+vim.keymap.set("n", "<leader>t", "<CMD>Neotree<CR>", { desc = "Open Neotree" })
