11fa3b2fdb4c05360e103b5ea9aad333a7443d06
iancleary <github@iancleary.me>
2025-10-21 17:05:20 +0000

    Update nixpkgs target

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "iancleary's terminal configuration (neovim, git, zsh, etc.)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     ## neovim-plugins
