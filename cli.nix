ffa29ea5861f092b14b2f6aee689a74924140e50
iancleary <github@iancleary.me>
2025-10-22 20:21:17 -0700

    Remove bun and pnpm from nix config, use npm -g

--- cli.nix
+++ cli.nix
@@ -45,10 +45,10 @@ in
       yj
       yq
 
-      # core
+      # software
       nodejs_22
-      unstable.pnpm
-      unstable.bun
+      # for bun, pnpm, use npm - `npm install -g bun pnpm`'
+      # since this allows them to stay up to date more easily
       rustup
       unstable.uv
     ];
